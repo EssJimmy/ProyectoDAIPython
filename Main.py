45794c9b66154637c36c9b6829aa89f1a12f2541
Jimmy <jperez81@itam.mx>
2023-04-27 12:45:54 -0600

    Ya se resolvieron todos los problemas menos el último ya que lo entendí mal, las correcciones se harán a su debido tiempo

--- Main.py
+++ Main.py
@@ -3,10 +3,11 @@ import Proyecto as Pr
 
 directory = "C:\\Users\\jaime\\Documents\\ITAM\\Desarrollo de aplicaciones informáticas\\Python\\Equipo1\\"
 df_amazon = Pr.read_file(directory, "Amazon.csv")
-df_amex = Pr.read_file(directory, "AmericanExpress.csv")
-df_astra = Pr.read_file(directory, "AstraZeneca.csv")
-df_danone = Pr.read_file(directory, "Danone.csv")
-df_netflix = Pr.read_file(directory, "Netflix.csv")
 
 print(df_amazon)
 print(Pr.find_month_values(df_amazon))
+Pr.total_transactions(df_amazon)
+Pr.earnings(df_amazon)
+print(df_amazon)
+
+Pr.plot_shares(df_amazon, "Close/Last")
--- Proyecto.py
+++ Proyecto.py
@@ -5,13 +5,15 @@ import matplotlib.pyplot as plt
 def read_file(directory: str, file_name: str):
     if file_name.endswith(".csv"):
         file = pd.read_csv(directory+file_name, encoding="UTF-8", header=0)
-        data = sort_by_date(file)
+        data = format_file(file)
         return data
     else:
         print("Selected file is not a .csv file!")
 
 
-def sort_by_date(data: pd.DataFrame):
+def format_file(data: pd.DataFrame):
+    data["Close/Last"] = data["Close/Last"].str.strip("$")
+    data["Close/Last"] = data["Close/Last"].astype(float)
     data["Date"] = pd.to_datetime(data["Date"], format="%m/%d/%Y")
     data.sort_values(by="Date", inplace=True)
 
@@ -20,10 +22,6 @@ def sort_by_date(data: pd.DataFrame):
 
 # Problema 1, 3 y 5 (solo cambiamos el nombre de la columna del que queremos estos valores)
 def find_month_values(data: pd.DataFrame, column="Close/Last"):
-    if column == "Close/Last":
-        data[column] = data[column].str.strip("$")
-        data[column] = data[column].astype(float)
-
     avg_df = data
     avg_df["month"] = pd.to_datetime(avg_df["Date"]).dt.month
     avg_df["year"] = pd.to_datetime(avg_df["Date"]).dt.year
@@ -36,23 +34,47 @@ def find_month_values(data: pd.DataFrame, column="Close/Last"):
 
 # Problema 2
 def total_transactions(data: pd.DataFrame):
+    data["Volume"] = data["Volume"].astype(float)
     data["Total"] = data["Close/Last"] * data["Volume"]
 
 
 # Problema 4
 def earnings(data: pd.DataFrame):
+    data["Open"] = data["Open"].str.strip("$")
+    data["Open"] = data["Open"].astype(float)
     data["Ganancias"] = data["Open"]/(data["Close/Last"] - 1)
 
 
 # Problema 6
+# Esto tiene que ser cambiado por completo, ya que estoy haciendo un ejercicio que no me pidieron
+# Lo que pide es que ploteemos el promedio, maximo y minimo de tres columnas, no de todas las columnas hay
+# del único que tenemos que plotear histórico es de la ganancia, pero eso ya está ez
 def plot_shares(data: pd.DataFrame, column: str):
     values = []
+    x = []
 
     if column == "Close/Last" or "Total" or "Ganancia":
         max_value, min_value, avg_value = find_month_values(data, column)
-        values.append(data.index.get_loc(max_value))
-        values.append(data.index.get_loc(min_value))
-        values.append(data.index.get_loc(avg_value))
+        for value in max_value["Close/Last"].astype(float).values:
+            values.append(value)
 
-    data[column].plot(x=column, y="Valor", title=column, markevery=values)
+        for value in min_value["Close/Last"].astype(float).values:
+            values.append(value)
+
+        for value in values:
+            index = find_index_values(data, column, value)
+            for i in index:
+                x.append(i)
+
+        for value in avg_value["Close/Last"].astype(float).values:
+            values.append(value)
+
+    print(len(x))
+    print(len(values))
+    data[column].plot(x=column, y="Valor", title=column)
+
     plt.show()
+
+
+def find_index_values(data: pd.DataFrame, column: str, value: float):
+    return data[data[column] == value].index.values.tolist()
